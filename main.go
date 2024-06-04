9e5fd8c137a7f8ff39d640e3580f11790fba346e
yasasvi <yasasvigumma@gmail.com>
2024-06-04 11:52:47 +0530

    client server is adjusted according to dev server

--- main.go
+++ main.go
@@ -13,25 +13,19 @@ import (
 )
 
 func main() {
-	// Load environment variables from .env file
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found or error loading .env file")
 	}
 
-	// Serve files from the ./pages directory
 	fs := http.FileServer(http.Dir("./pages"))
 	http.Handle("/", fs)
 
-	// Get the port from environment variables or use default
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	// Create the server
 	server := &http.Server{Addr: ":" + port, Handler: nil}
-
-	// Start the server in a goroutine
 	go func() {
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("ListenAndServe(): %v", err)
@@ -40,8 +34,6 @@ func main() {
 
 	fmt.Println("Server is running on http://localhost:" + port)
 	fmt.Println("Press 'q' and 'Enter' to stop the server")
-
-	// Create a scanner to read input from stdin
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		// Read input
@@ -51,18 +43,14 @@ func main() {
 			break
 		}
 	}
-
 	if err := scanner.Err(); err != nil {
 		log.Printf("Error reading from input: %v", err)
 	}
 
-	// Attempt to gracefully shut down the server
 	fmt.Println("Shutting down the server...")
 	if err := server.Close(); err != nil {
 		fmt.Printf("Error shutting down server: %v\n", err)
 	}
-
-	// Give some time for the server to shut down gracefully
 	time.Sleep(1 * time.Second)
 	fmt.Println("Server stopped.")
 }
